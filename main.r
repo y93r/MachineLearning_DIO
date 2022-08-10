c979c498a8a020c4acb24246fff3541de0077d4a
YarA <104739868+y93r@users.noreply.github.com>
2022-08-10 14:17:38 +0000

    Initial commit

---
+++ main.r
@@ -0,0 +1,13 @@
+obj1 <- 3*3
+obj2 <- 8/2
+obj3 <- obj1 - obj2
+
+obj3
+
+raio <- 10
+espessura <- 1
+comprimento <- 70
+#pi já é uma variável do R
+
+volume <- pi*(raio - espessura)^2*comprimento #volume do cubo
+print(volume)
